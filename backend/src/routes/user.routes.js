f14e41daa0eb2b1be406ea28ad202907b049fd2d
hfcosta <hfcosta>
2026-08-13 13:58:35 -0300

    correções user.routes e planejamento de organização do frontend

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,9 @@
-const express = require('express');
+import express from "express";
+import UserController from "../controllers/user.controller.js";
+
 const router = express.Router();
-const userController = require('../controllers/user.controller');
 
-router.get('/', userController.getUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
-module.exports = router;
+export default router;
